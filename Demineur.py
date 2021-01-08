e746e083f3ecf4f1816bd4651187453233a892f8
brillate <emma.brillat@edu.esiee.fr>
2021-01-08 07:09:20 +0100

    commit final

--- Demineur.py
+++ Demineur.py
@@ -1,6 +1,8 @@
 import random
 import math
+import sys
 from copy import deepcopy
+
 INCONNU = '_'
 PERDU = '!'
 DRAPEAU = '*'
@@ -9,7 +11,7 @@ LEVELS = [(9,9), (16,16), (24,24)]
 MINES = [10, 40, 100]
 
 
-def genere_plateau_vide(level): #level = 0 ou 1 ou 2
+def genere_plateau_vide(level): # Level = 0/1/2
     x_size, y_size = LEVELS[level]
     plateau_vide = []
     for i in range(y_size):
@@ -18,29 +20,25 @@ def genere_plateau_vide(level): #level = 0 ou 1 ou 2
             plateau_vide[i].append({"mine" : False, "etat" : INCONNU}) 
     return plateau_vide
 
-
-
 def place_mines(plateau, level, alea=True):
     x_size,y_size = LEVELS[level]
-    #plateau_avec_mines = place_mines(plateau, level) # alea=True
-    #plateau_avec_mines = place_mines(plateau, level, alea = False) 
-    
+    # Plateau_avec_mines = place_mines(plateau, level) # alea=True
+    # Plateau_avec_mines = place_mines(plateau, level, alea = False) 
     n = MINES[level]
     plateau_avec_mines = deepcopy(plateau)
     if not alea:
-        # on place les mines sur la diagonale
+    # On place les mines sur la diagonale
         for i in range(x_size):
             plateau_avec_mines[i][i]['mine'] = True
         return plateau_avec_mines
-    # on place les mines de façon aléatoire
-    
+
+    # On place les mines de façon aléatoire
     while n!=0:
-            i = random.randint(0,x_size-1)
-            j = random.randint(0,y_size-1)
-            if not plateau[i][j]['mine']:
-                plateau_avec_mines[i][j]['mine'] = True
-                n-=1
-            
+        i = random.randint(0,x_size-1)
+        j = random.randint(0,y_size-1)
+        if not plateau[i][j]['mine']:
+            plateau_avec_mines[i][j]['mine'] = True
+            n-=1       
     return plateau_avec_mines
 
 def construire_plateau(level, alea = True):
@@ -48,9 +46,8 @@ def construire_plateau(level, alea = True):
     plateau = place_mines(plateau_vide, level, alea)
     return plateau
 
-
 def coup_joueur(plateau):
-    #le joueur donne (x,y) ou il met un drapeau
+    # Le joueur donne (x,y) ou met un drapeau
     answer = input("Coordonnées ? : ")
     x, y = answer.split(',')
     d = input("Un DRAPEAU ? oui/non : ")
@@ -63,19 +60,19 @@ def coup_joueur(plateau):
         return (int(x),int(y))
 
 def case_voisines(plateau,x,y):
-        hauteur = len(plateau)
-        largeur = len(plateau[0])
-        l = []
-        for i in range(-1,2):  #case restantes au centre du plateau
-            for j in range(-1,2):
-                l.append([x+i,y+j])   
-        l.remove([x,y])
-        l2=[]
-        for k in l:
-            if k[0] >= 0 and k[1] >= 0:
-                if k[0] < hauteur and k[1]< largeur:
-                    l2.append(k)
-        return l2   
+    hauteur = len(plateau)
+    largeur = len(plateau[0])
+    l = []
+    for i in range(-1,2):  
+        for j in range(-1,2):
+            l.append([x+i,y+j])   
+    l.remove([x,y])
+    l2=[]
+    for k in l:
+        if k[0] >= 0 and k[1] >= 0:
+            if k[0] < hauteur and k[1]< largeur:
+                l2.append(k)
+    return l2   
 
 def compte_mines_voisines(plateau,x,y):
     compt = 0
@@ -87,26 +84,18 @@ def compte_mines_voisines(plateau,x,y):
     return compt
     
 def composante_connexe(plateau,x,y):
-    # met le plateau à jour en ouvrant toutes les cases vides à partir de la case (x,y)
-    #il d'agit d'une procédure, il s'agir d'un bloc d'instructions qui ne renvoie pas de valeur à la fin.
-    #ici la variable plateau est modifié.
-    
+    # Met le plateau à jour en ouvrant toutes les cases vides à partir de la case (x,y)
     if plateau[x][y]['etat'] != INCONNU:
         return
     L = case_voisines(plateau,x,y)
-    
     plateau[x][y]['etat'] = compte_mines_voisines(plateau,x,y)
     if plateau[x][y]['etat'] != 0:
         return
-    
     else:
         for i in L:
             composante_connexe(plateau,i[0],i[1])
-
     
 def decouvre_case(plateau,x,y):
-    "fonction et procédure a la fois car renvoie un booléen et modifie l'argument plateau. renvoie False si la case contenait une mine et True sinon."
-
     if plateau[x][y]["mine"] == True and plateau[x][y]["etat"] != DRAPEAU:
         plateau[x][y]["etat"] = PERDU
         print("Désolé, vous avez perdu ! ")
@@ -128,9 +117,8 @@ def compte_mine_solution(plateau):
                 plateau[x][y]["etat"] = compte_mines_voisines(plateau,x,y)
     return plateau
 
-
 def total_mines(plateau):
-    #Compte le nombre total de mines sur le plateau.
+    # Compte le nombre total de mines sur le plateau.
     c = 0
     for i in plateau:
         for j in i:
@@ -140,7 +128,7 @@ def total_mines(plateau):
 
 
 def check(plateau):
-    #compte le nombre de drapeau et de case inconnue
+    # Compte le nombre de drapeau et de case inconnue
     c = 0
     for i in plateau:
         for j in i:
@@ -149,7 +137,7 @@ def check(plateau):
     return c
 
 def display(plateau):
-    #permet d'afficher la grille
+    # Permet d'afficher la grille
     for ligne in plateau:
         for colonne in ligne:
             print(colonne['etat'], ' ',end="")
@@ -158,27 +146,29 @@ def display(plateau):
     
 def write_score(filename, score):
     with open(filename, mode='a', encoding='utf8') as f:
-        f.write('Parties gagnées : ')
-        f.write('\n')
         f.write(str(score))
         f.write('\n')
-    
 
 def read_scores(filename):
     with open(filename, mode='r', encoding='utf8') as f:
         scores = f.readlines()
     return scores
 
+def fin_de_jeu(score):
+    write_score(filename,score)
+    print( "Merci d'avoir joué ! ")
+    sys.exit()
+
+def choix():
+    niv = input("Niveau 0/1/2/Q ? : ")
+    return niv 
+
 filename = 'scores.txt'
 
-scores = read_scores(filename)
+score = [0,0,0]
 
-
-write_score(filename,0)
-
-
-niv = input("Niveau 0/1/2 ? : ")
-plateau=construire_plateau(int(niv), alea = False )
+niv = choix()
+plateau=construire_plateau(int(niv), alea = True )
 m = total_mines(plateau)
 print(m)
 
@@ -190,22 +180,28 @@ while True:
         print(m)
     else : print(m)
     L = case_voisines(plateau,x,y)
-    
-    if not decouvre_case(plateau,x,y):   
+    if not decouvre_case(plateau,x,y): # si le joueur à perdu
+        score[1] = score[1] + 1
+        score[2] = score[0]/score[1]*100
         plateau = compte_mine_solution(plateau)
         display(plateau)
-        niv = input("Niveau 0/1/2 ? : ")
-        plateau = construire_plateau(int(niv), alea = False)
+        niv = choix()
+        if niv =='Q':
+            fin_de_jeu(score) 
+        plateau = construire_plateau(int(niv), alea = True)
         m = total_mines(plateau) 
         print(m)                                        
-    if total_mines(plateau) == check(plateau):
+    if total_mines(plateau) == check(plateau): # si le joueur à gagné  
+        score[0] = score[0] + 1
+        score[2] = score[0]/score[1]*100
         print("Bravo, tu as gagné ! ")
-        write_score(filename, int(scores[-1]) + 1)
-        niv = input("Niveau 0/1/2 ? : ")
-        plateau = construire_plateau(int(niv), alea = False)
+        niv = choix()
+        if niv == 'Q':
+            fin_de_jeu(score)
+        plateau = construire_plateau(int(niv), alea = True)
         m = total_mines(plateau) 
         print(m)
-    
+
     
     
         
--- README.md
+++ README.md
@@ -6,15 +6,15 @@ Le but du jeu du démineur est de découvrir toutes les cases vides du plateau s
 ***
 Ici, le plateau est représenté par un liste de liste de dictionnaire.
 
-La case de coordonnée (x,y) est un dictionnaire donc une paire clé valeur.
+La case de coordonnée (x,y) est un dictionnaire (une paire clé valeur).
   - "mine" (une clé) qui est un booléen et qui indique si la case contient une mine 
-  - "etat" (une clé) indique l'état de la case :
+  - "etat" (une clé) indique l'état de la case (la valeur) :
       - INCONNU quand le joueur n'a pas découvert la case
       - Un entier compris entre 0 et 8 qui indique le nombre de mines voisines, quand le joueur a decouvert la case.
       - PERDU quand il s'agit d'une case avec une mine, que le joueur a séléctionnée.
       - DRAPEAU si le joueur a mis un drapeau.
 
-Pour rendre le jeu plus visuel on attribut les symboles suivant à ces étiquettes :  
+Pour rendre le jeu plus visuel on attribut les symboles suivant aux différents état de la case :  
 
 - INCONNU = _
 - PERDU = !
@@ -26,7 +26,7 @@ Pour rendre le jeu plus visuel on attribut les symboles suivant à ces étiquett
 - Niveau 3 --> Expert : plateau de taille 24*24 avec 100 mines.
 
 A chaque partie le joueur décide du niveau qu'il veut.
-Dans le fichier scores.txt on stock les scores. Au démarrage le score du joueur est de 0 puis il augmente de 1 à chaque fois que le joueur gagne une partie.
+Dans le fichier scores.txt on stock les scores sous forme de liste à trois éléments. Le premier élément correspond au nombre de partie gagnée, le deuxième à celui des parties perdues et le troisième au porcentage de partie gagnée. L'écriture dans le fichier se fait à chaque fois que le joueur quitte le jeu (ferme le jeu).
 
 ### Collaboration
 Le projet à été mené à bien par Emma Brillat, pour l'évaluation de l'unité IGI-3008. Début du travail en octobre 2020, fin en janvier 2021.
@@ -42,7 +42,9 @@ Et si alea = False, place n mines sur la diagonale.
 
 La fonction `construire_plateau(level, alea = True)` génère le plateau.
 
-la fonction `coup-joueur(x,y)` demande au joueur quel niveau de jeu il veut, les coordonnées de la case qu'il veut découvrir et s'il veut y mettre un drapeau.
+La fonction `choix()` demande au joueur à quel niveau de jeu il veut jouer ou s'il veut quitter le jeu.
+
+la fonction `coup-joueur(x,y)` demande au joueur les coordonnées de la case qu'il veut découvrir et s'il veut y mettre un drapeau.
 
 La fonction `case_voisines(plateau,x,y)` renvoie une liste de liste de coordonnées des cases voisine de la case (x,y).La liste est donc de taille 3 ou 5 ou 8.
 
@@ -54,7 +56,9 @@ La fonction `check(plateau)` retourne le nombre de case inconnue et de case avec
 
 La fonction `display(plateau)` permet d'afficher le plateau sous forme de grille et non pas de liste. Et ainsi de rendre la partie de jeu beaucoup plus visuelle.
 
-Les fonctions `write_score(filename,score)` et `read_scores(filename)` permettent respectivement d'écrire les scores dans le fichier scores.txt et de les lire.
+Les fonctions `write_score(filename,score)` et `read_scores(filename)` permettent respectivement d'écrire et de lire les scores dans le fichier scores.txt.
+
+La fonction `fin_de_jeu(score)` quitte le jeu si le joueur veut arrêter de jouer.
 
 #### Appel des fonctions
 
@@ -68,8 +72,8 @@ Le déroulement d'une partie de jeu est la suivante :
 3) Choix de la case à découvrir ou mettre un drapeau.
 
 4) 
-  - Si elle contenait une mine alors le joueur à perdu. Et recommence une partie.
-  - Si le nombre totale de mine sur le plateau est égal a celui du nombre de drapeau + de case inconnu alors le joueur à gagné et son score augmente de 1.
+  - Si elle contenait une mine alors le joueur à perdu et le nombre de partie perdue augmente de 1. Il Peut recommencer une partie si il veut.
+  - Si le nombre totale de mine sur le plateau est égal a celui du nombre de drapeau + de case inconnu alors le joueur à gagné et le nombre de partie gagné augmente de 1.
   - Sinon on compte le nombre de mines qu'il y a autour de cette case.
       - Et si il n'y en a pas on continue de découvrir les cases voisines des cases voisines.
       - Sinon on s'arrête.
@@ -78,6 +82,8 @@ Le déroulement d'une partie de jeu est la suivante :
 
 6) Choix de la case à découvir ou mettre un drapeau.
 
-7) Refait les étapes 4), 5), 6) jusqu'à l'infini.
+7) Refait les étapes 4), 5), 6) jusqu'à ce que le joueur quitte le jeu.
+
+8) Lorsque le joueur quitte la partie son score est écrit dans le fichier.
 
 
